3040ec971cc36c87e81d1650582aa7646a005610
simen.bergh <simen.bergh@smartoptics.com>
2024-11-07 16:11:45 +0100

    Update movie-service.cs

--- Services/Interfaces/movie-service.cs
+++ Services/Interfaces/movie-service.cs
@@ -5,15 +5,26 @@ using MovieCharactersAPI.Services.Interfaces;
 
 namespace MovieCharactersAPI.Services
 {
+    /// <summary>
+    /// Service class for managing movies.
+    /// </summary>
     public class MovieService : IMovieService
     {
         private readonly MovieCharactersDbContext _context;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MovieService"/> class.
+        /// </summary>
+        /// <param name="context">The database context.</param>
         public MovieService(MovieCharactersDbContext context)
         {
             _context = context;
         }
 
+        /// <summary>
+        /// Gets all movies asynchronously.
+        /// </summary>
+        /// <returns>A list of movies.</returns>
         public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
         {
             return await _context.Movies
@@ -22,6 +33,11 @@ namespace MovieCharactersAPI.Services
                 .ToListAsync();
         }
 
+        /// <summary>
+        /// Gets a movie by its identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The movie identifier.</param>
+        /// <returns>The movie with the specified identifier.</returns>
         public async Task<Movie> GetMovieByIdAsync(int id)
         {
             return await _context.Movies
@@ -30,6 +46,11 @@ namespace MovieCharactersAPI.Services
                 .FirstOrDefaultAsync(m => m.Id == id);
         }
 
+        /// <summary>
+        /// Adds a new movie asynchronously.
+        /// </summary>
+        /// <param name="movie">The movie to add.</param>
+        /// <returns>The added movie.</returns>
         public async Task<Movie> AddMovieAsync(Movie movie)
         {
             _context.Movies.Add(movie);
@@ -37,6 +58,12 @@ namespace MovieCharactersAPI.Services
             return movie;
         }
 
+        /// <summary>
+        /// Updates an existing movie asynchronously.
+        /// </summary>
+        /// <param name="id">The identifier of the movie to update.</param>
+        /// <param name="movie">The updated movie data.</param>
+        /// <returns>The updated movie.</returns>
         public async Task<Movie> UpdateMovieAsync(int id, Movie movie)
         {
             var existingMovie = await _context.Movies
@@ -58,6 +85,10 @@ namespace MovieCharactersAPI.Services
             return existingMovie;
         }
 
+        /// <summary>
+        /// Deletes a movie by its identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The identifier of the movie to delete.</param>
         public async Task DeleteMovieAsync(int id)
         {
             var movie = await _context.Movies.FindAsync(id);
@@ -68,11 +99,21 @@ namespace MovieCharactersAPI.Services
             }
         }
 
+        /// <summary>
+        /// Checks if a movie exists by its identifier asynchronously.
+        /// </summary>
+        /// <param name="id">The movie identifier.</param>
+        /// <returns>True if the movie exists, otherwise false.</returns>
         public async Task<bool> MovieExistsAsync(int id)
         {
             return await _context.Movies.AnyAsync(m => m.Id == id);
         }
 
+        /// <summary>
+        /// Updates the characters associated with a movie asynchronously.
+        /// </summary>
+        /// <param name="movieId">The identifier of the movie.</param>
+        /// <param name="characterIds">The list of character identifiers to associate with the movie.</param>
         public async Task UpdateMovieCharactersAsync(int movieId, List<int> characterIds)
         {
             var movie = await _context.Movies
@@ -95,6 +136,11 @@ namespace MovieCharactersAPI.Services
             await _context.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Gets the characters associated with a movie asynchronously.
+        /// </summary>
+        /// <param name="movieId">The identifier of the movie.</param>
+        /// <returns>A list of characters in the movie.</returns>
         public async Task<IEnumerable<Character>> GetCharactersInMovieAsync(int movieId)
         {
             var movie = await _context.Movies
